b9d82f19ce0b4efe3d78ddaebe20f256f48e1443
nurai <nuraimaratova50@gmail.com>
2020-10-09 12:10:48 +0600

    remove comments in comments/urls.py

--- comment_service/comments/urls.py
+++ comment_service/comments/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('create/', CommentCreate.as_view(), name='create'),
     path('<int:pk>/update/', CommentUpdate.as_view(), name='update'),
     path('<int:pk>/delete/', CommentDelete.as_view(), name='delete'),
-    # path('posts/<str:slug>/', post_detail, name='post_detail'),
-    # path('export_comments/', export, name='export_comments'),
-    # path('export_comments_excel/', export_comments_xls, name='export_comments_excel')
 ]
