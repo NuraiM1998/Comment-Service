d3383e03c7f8ebc6a0bf7fca364988ccd3687aa8
nurai <nuraimaratova50@gmail.com>
2020-10-26 15:50:47 +0600

    remove redundant comments

--- comment_service/comments/models.py
+++ comment_service/comments/models.py
@@ -79,28 +79,3 @@ class Node(ClosureModel): # пока что не используется(воз
                             on_delete=models.CASCADE, 
                             related_name='nodes', 
                             null=True)
-
-
-# @receiver(post_save, sender=PostComment)
-# def save_hierarchy(sender, instance=None, created=False, **kwargs):
-#     print('instance: ', instance)
-#     print('instance.parent: ',instance.parent)
-#     # print('instance.child: ',instance.child)
-#     if created:
-#         self_comment = CommentHierarchy.objects.create( # текущий коммент
-#                                         parent=instance,
-#                                         child=instance)
-#         if instance.parent: # если у комента есть родитель
-#             parent_hierarchy = CommentHierarchy.objects.filter( # фильтруем родителей которые не вложенны(root)
-#                                                     child=instance.parent,
-#                                                     depth__gt=0)
-#             CommentHierarchy.objects.create(parent=instance.parent,
-#                                             child=instance,
-#                                             depth=1)
-#             for node in parent_hierarchy:
-#                 CommentHierarchy.objects.create(
-#                                     parent=node.parent,
-#                                     child=instance,
-#                                     depth=node.depth+1)
-#                 print('node: ', node)
-#                 print('node.parent: ', node.parent)
